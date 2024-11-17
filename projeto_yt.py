0c9175bc2952dd531e826722df19ec8d78f116be
AxelWill <63531015+AxelWill@users.noreply.github.com>
2024-11-16 22:36:48 -0300

    Update projeto_yt.py
    Comentários no código.

--- projeto_yt.py
+++ projeto_yt.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 #pip install --upgrade google-api-python-client
 
 #from googleapiclient.discovery import build
@@ -7,7 +5,10 @@
 #import time
 
 def Registro(log):
+    #O código criará um log com as informações do vídeo, e irá registra-lo num arquivo de texto
+    #O código irá mostrar no console as mesmas coisas que registrar no arquivo.
     data_atual = datetime.today().strftime("%D %H:%M:%S")
+    #A data e o horário serão adicionados ao relátorio.
     arq = open('log.txt', 'a')
     arq.write(data_atual +'\n'+ log +'\n\n')
     arq.close()
@@ -16,6 +17,7 @@ def Registro(log):
     return
 
 youtube = build('youtube','v3',developerKey='INSIRA SUA CHAVE API')
+#O programa requer que o usuário insira sua própria chave API
 
 def AnaliseYT(ytID, tempo, delay):
   inicio = time.time()
@@ -40,26 +42,29 @@ def AnaliseYT(ytID, tempo, delay):
 
     view=estatisticas['viewCount']
     like=estatisticas['likeCount']
-    favoritos = estatisticas['favoriteCount']
-    comentarios = estatisticas['commentCount']
+    #favoritos = estatisticas['favoriteCount']
+    #comentarios = estatisticas['commentCount']
 
     saida = f'''Quantidade de visualizações: {view}
 Quantidade de curtidas: {like}
 Quantidade de comentários: {comentarios}\n'''
-    Registro(saida)
+    Registro(saida) 
 
     time.sleep(delay)
-
-    #End of For
+    #final do for
 
   difView = int(view)-viewInicial
   difLike = int(like)-likeInicial
   delta = (time.time() - inicio)/60
 
   diff = f'A diferença de visualizações ao longo da análise foi de {difView}, e a de curtidas foi {difLike} ao longo de {delta} minutos'
-
+    #No final do processo o código ira dizer e registrar no arquivo a diferença das estatísticas do vídeo do início até a última análise, além do tempo decorrido do programa.
   Registro(diff)
+    
   return(Registro('Coleta de dados concluída\n'))
 
-ytID = 'ay2ZPX8vgG8'#Insira a parte final do link do vídeo do youtube que quiser analisar
+ytID = 'olDCJ1w3FLM'
+#Insira a parte final do link do vídeo do youtube que quiser analisar
+
 AnaliseYT(ytID, 60, 10)
+#O usúario então define o tempo, em minutos, que o programa vai ficar coletando os dados, e também o intervalo entre cada coleta. No final de cada intervalo será registrado num arquivo .txt os valores de curtidas e visualizações do vídeo, além do momento em que os dados foram coletados.
